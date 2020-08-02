0b77052e9a9dc9c200e17f16ba3d5f79017e2280
NeoStyx <67185896+nsde@users.noreply.github.com>
2020-08-02 12:07:01 +0200

    Create 01-helloworld.py

---
+++ Python/Anfänger/01-helloworld.py
@@ -0,0 +1,27 @@
+# MEIN ERSTES PROGRAMM
+
+# eine einzeilige Notiz, die bis zum Ende der Zeile geht!
+
+'''
+eine
+mehrzeilge
+Notiz
+'''
+
+print('Hallo, Welt!') # Befehl zum Ausgeben des Textes "Hallo, Welt!" auf dem Terminalfenster.
+print("Und Hallo, Python!") # Der Nächste Befehl, dieses Mal mit "Anführungszeichen" statt 'Apostrophen'.
+
+print('Neue\nZeile!') # "\n" definiert einen Zeilenumbruch: damit können wir 2 oder mehr Zeilen auf einmal ausgeben - ohne neue Befehle zu schreiben.
+print('\\n') # Wenn wir keinen Zeilenumbruch haben wollen, aber "\n" schreiben wollen, benötigen wir zweimal das Backslash-Zeichen.
+
+print('Eingerückter\tText') # "\n" definiert eine Tabulatortaste, die einen kleinen Abstand einfügt.
+print('\\t') # Hier gilt dasselbe, wie oben: zweimal das Backslash-Zeichen, und schon können wir den Text "\t" ohne Zeilenumbruch schreiben.
+
+# Wenn man zu wenig Platz auf dem Bildschirm hast, wird unter anderem das "\"-Zeichen verwendet, und den Code übersichtlicher zu gestalten.
+# Der Befehl geht einfach in der nächsten Zeile weiter:
+print('Mein Bildschirm ist\
+klein!')
+
+# Alternativ kann man auch die praktische Syntax nutzen:
+print('Mein Bildschirm ist
+klein!')

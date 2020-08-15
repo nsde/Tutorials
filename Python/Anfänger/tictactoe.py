f5a0ffd38ddd119aa12f1f3c98cb51855c53a737
Felix <67185896+nsde@users.noreply.github.com>
2020-08-15 17:46:48 +0200

    Create tictactoe.py

---
+++ Python/Anfänger/tictactoe.py
@@ -0,0 +1,36 @@
+from random import randint as rdi
+import sys
+
+l = ['-','-','-','-','-','-','-','-','-']
+
+end = False
+
+def run():
+    for row in range(3):
+        for col in range(3):
+            print(end=l[col])
+        print(end='\n')
+
+    inp = input('\n>')
+
+    try:
+        inp = int(inp)
+        if inp > -1 and inp < 9:
+            pass
+        else:
+            print(Error)
+
+    except:
+        if inp == 'end':
+            end = True
+            return end
+        else:
+            print('ERROR')
+
+# print tictactoe
+
+while 1:
+    if end:
+        sys.exit(0)
+    else:
+        run()

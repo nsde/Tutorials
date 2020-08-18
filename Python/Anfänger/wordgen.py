76f1b589fb5a9d8e9c34cdaf268b261fe0e8703c
nsde <67185896+nsde@users.noreply.github.com>
2020-08-18 16:56:13 +0200

    3

--- Python/Anfänger/wordgen.py
+++ Python/Anfänger/wordgen.py
@@ -2,13 +2,20 @@
 # silben-generation
 
 from random import randint as rdi
+from random import choice as c
 
-s = 'dfgklmprtw' # "easy" words
+l = 'dfklmprtw' # letters (prefixes of the syllables)
+v = 'aeiou'# vocals (suffixes of the syllables)
+
+min_gen = 2 # minimum syllables per word
+max_gen = 4 # maximal syllables per word
+
+word_count = 10 # no. of words to generate
 
 def word():
-    for _  in range(rdi(4,8)):
-        print(end=s)
+    for _  in range(rdi(min_gen,max_gen)):
+        print(end=c(l) + c(v)) # choose random and generate
 
-for _ in range(10):
+for _ in range(word_count):
     word()
     print(end='\n')
